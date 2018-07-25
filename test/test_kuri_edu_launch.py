e3b4b09ef9ea7ca1796aa29d800c3adff9c428f8
Peter Baughman <peter@mayfieldrobotics.com>
2018-07-25 17:06:19 +0000

    Test that the launch files are OK for kuri_edu

---
+++ test/test_kuri_edu_launch.py
@@ -0,0 +1,20 @@
+import roslib
+import roslaunch
+
+from nose.plugins.attrib import attr
+
+
+def check_launch_file(file_name):
+    lf_path = roslib.packages.find_resource(
+        "kuri_edu",
+        file_name
+    )[0]
+
+    loader = roslaunch.xmlloader.XmlLoader()
+
+    loader.load(lf_path, roslaunch.config.ROSLaunchConfig())
+
+
+@attr("gbz")
+def test_kuri_launch():
+    check_launch_file("kuri_edu.launch")
