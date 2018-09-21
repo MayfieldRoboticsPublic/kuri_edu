64ff6a22ff65d26afae517b787b66ed3ebd341eb
Brad Greer <brad@mayfieldrobotics.com>
2018-09-21 10:32:56 -0700

    Corrected handedness reversal, lowered intensity of center lights

--- kuri_edu/src/kuri_edu/cap_touch_chest_light.py
+++ kuri_edu/src/kuri_edu/cap_touch_chest_light.py
@@ -9,19 +9,19 @@ from mobile_base import ChestLightClient as clc
 class CapTouchChestLed(object):
 
     # Map touch inputs to LED outputs
-    TOUCH_TO_LIGHT = ([clc.IDX_OUTER_BOTTOM_MID_RIGHT],
-                      [clc.IDX_OUTER_UPPER_MID_RIGHT],
-                      [clc.IDX_OUTER_UPPER_TOP_RIGHT],
+    TOUCH_TO_LIGHT = ([clc.IDX_OUTER_BOTTOM_MID_LEFT],
+                      [clc.IDX_OUTER_UPPER_MID_LEFT],
+                      [clc.IDX_OUTER_UPPER_TOP_LEFT],
                       range(clc.IDX_INNER_LEFT+1),
                       [clc.IDX_OUTER_BOTTOM_LOW_LEFT, clc.IDX_OUTER_BOTTOM_LOW_RIGHT],
-                      [clc.IDX_OUTER_UPPER_TOP_LEFT],
-                      [clc.IDX_OUTER_UPPER_MID_LEFT],
-                      [clc.IDX_OUTER_BOTTOM_MID_LEFT])
+                      [clc.IDX_OUTER_UPPER_TOP_RIGHT],
+                      [clc.IDX_OUTER_UPPER_MID_RIGHT],
+                      [clc.IDX_OUTER_BOTTOM_MID_RIGHT])
 
     TOUCH_TO_COLOR = (clc.ON,
                       clc.ON,
                       clc.ON,
-                      clc.ON,
+                      clc.HALF,
                       clc.ON,
                       clc.ON,
                       clc.ON,
@@ -50,17 +50,17 @@ class CapTouchChestLed(object):
         )
 
     def run(self):
- 
+
         # Indicate to the world that we're running and ready to go:
         self._pub.publish(
             mayfield_msgs.msg.NodeStatus("cap_touch_chest_light", True)
         )
         rospy.spin()
- 
+
     def shutdown(self):
         self._light_client.shutdown()
         self._touch_sub.unregister()
- 
+
     def _touch_cb(self, msg):
         frame = [(0,0,0)] * clc.NUM_LEDS
         for i, electrode in enumerate(msg.electrodes):
