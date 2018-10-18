38a47a240056620db49bd12eb15b14de4af77cfe
Peter Baughman <peter@mayfieldrobotics.com>
2018-10-18 20:21:05 +0000

    Nav roughly working.  Tests aren't right yet

--- kuri_edu/rostest/test_nav_controller.py
+++ kuri_edu/rostest/test_nav_controller.py
@@ -3,8 +3,10 @@ import threading
 
 import rostest
 import rospy
+from tf.transformations import quaternion_from_euler
 
 import amcl.msg
+import geometry_msgs.msg
 import nav_msgs.msg
 
 import gizmo_hw_sim.srv
@@ -53,6 +55,11 @@ class TestNavController(maytest.desktop.RosTestBase):
             gizmo_hw_sim.srv.Dock
         )
 
+        cls.goal_pub = rospy.Publisher(
+            "move_base_simple/goal",
+            geometry_msgs.msg.PoseStamped
+        )
+
         # Wait for the DUT to be online before starting
         mayfield_utils.wait_for_nodes(['nav_controller'])
 
@@ -77,6 +84,38 @@ class TestNavController(maytest.desktop.RosTestBase):
         self.dock_srv(is_docked=True, is_charging=False)
 
         monitor.wait_for_message()
+
+    def test_03_test_autonomous_navigation(self):
+
+        initial_loc = self.get_gizmo_location()
+        rospy.loginfo("Kuri started at {}".format(initial_loc))
+
+        self.goal_pub.publish(
+            geometry_msgs.msg.PoseStamped(
+                pose=geometry_msgs.msg.Pose(
+                    # Launch file starts kuri at -4.5, -5.4.  Move fowrad 1m
+                    position=geometry_msgs.msg.Point(-3.5, -5.4, 0),
+                    orientation=geometry_msgs.msg.Quaternion(0, 0, 0, 1)
+                )
+            )
+        )
+
+        # Poll to see that Kuri moves to the new destination
+        rate = rospy.Rate(1)
+        start = rospy.get_time()
+
+        # Wait until we're done moving
+        while rospy.get_time() - start  < 15:
+            loc = self.get_gizmo_location()
+            print loc
+            if abs(loc[0] - 1) <= 0.1:
+                break
+            rate.sleep()
+
+        # Check that we navigated to where we want to go
+        final_loc = self.get_gizmo_location()
+        self.assertAlmostEquals(final_loc[0], 1, places=1)
+        self.assertAlmostEquals(final_loc[1], 0, places=1)
 
 
 if __name__ == '__main__':
